d2a59761e2a4f73673aa619e4ce4697e751df0b4
GABRIEL CAMILO DANTAS <33359624866@jpmorgan.sp>
2017-11-14 21:58:26 -0200

    segundo

--- InterfacePolimorfismo/ExemploInterface/Program.cs
+++ InterfacePolimorfismo/ExemploInterface/Program.cs
@@ -9,6 +9,17 @@ namespace ExemploInterface
         {
             Produto pr = new Produto(30, "Sabao", "Em pó", 30);
             Console.WriteLine(pr.Cadastro());
+
+            Console.WriteLine("\n");
+
+            Endereco endereco = new Endereco();
+            endereco.Logradouro = "Rua Nova Esperança";
+            endereco.Numero = "27";
+            endereco.Bairro = "Jardim sei lá";
+
+            PF PF = new PF(10, "Helena", "2356", "helena@gmail.com", "6553", endereco);
+
+            Console.WriteLine(PF.Cadastro());
         }
     }
 }
